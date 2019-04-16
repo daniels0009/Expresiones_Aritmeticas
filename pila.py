40b46fe9c32463ff3e376818f430d06f793d9475
daniels0009 <36423740+daniels0009@users.noreply.github.com>
2019-04-16 16:48:52 -0500

    Create pila.py

---
+++ pila.py
@@ -0,0 +1,25 @@
+clase  Pila :
+    "" " Representa una pila con operaciones de apilar, desapilar y
+        verificar si está vacía. ""
+ 
+    def  __init__ ( self ):
+        "" " Crea una pila vacía. " ""
+        # La pila vacía se representa con una lista vacía
+        auto .items = []
+
+    def  apilar ( self , x ):
+        "" " Agrega el elemento xa la pila. " ""
+        # Apilar es agregar al final de la lista.
+        auto .items.append (x)
+
+    def  desapilar ( uno mismo ):
+        "" Devuelve el elemento tope y lo elimina de la pila.
+            Si la pila está vacía levanta una excepción. ""
+        tratar :
+            volver  auto .items.pop ()
+        excepto  IndexError :
+            elevar  ValueError ( " La pila está vacía " )
+
+    def  es_vacia ( self ):
+        "" " Devuelve True si la lista está vacía, False si no. " ""
+        volver  auto .items == []
