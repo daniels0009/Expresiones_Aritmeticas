9401117b29cff290a16d3c78d2dbd7c6ab04cb4c
daniels0009 <36423740+daniels0009@users.noreply.github.com>
2019-04-16 16:47:49 -0500

    Create Expresiones.py

---
+++ Expresiones.py
@@ -0,0 +1,30 @@
+de pila import  *
+de arbol import  *
+def construirArbolAnalisis(expresionAgrupada):
+    listaSimbolos = expresionAgrupada.split()
+    pilaPadres = Pila()
+    arbolExpresion = ArbolBinario('')
+    pilaPadres.incluir(arbolExpresion)
+    arbolActual = arbolExpresion
+    for i in listaSimbolos:
+        if i == '(':
+            arbolActual.insertarIzquierdo('')
+            pilaPadres.incluir(arbolActual)
+            arbolActual = arbolActual.obtenerHijoIzquierdo()
+        elif i not in ['+', '-', '*', '/', ')']:
+            arbolActual.asignarValorRaiz(int(i))
+            padre = pilaPadres.extraer()
+            arbolActual = padre
+        elif i in ['+', '-', '*', '/']:
+            arbolActual.asignarValorRaiz(i)
+            arbolActual.insertarDerecho('')
+            pilaPadres.incluir(arbolActual)
+            arbolActual = arbolActual.obtenerHijoDerecho()
+        elif i == ')':
+            arbolActual = pilaPadres.extraer()
+        else:
+            raise ValueError
+    return arbolExpresion
+
+MiArbol= construir['(', '20','/','2',') '+', '(', 100', '*', '200' ,')',')')']
+print(MiArbol) 
